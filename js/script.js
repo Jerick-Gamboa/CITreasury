1ff438bfaac339e895fd79f5d02051ebcd9bc7d6
jhzrmx <dj.jasper128@gmail.com>
2024-05-06 13:20:42 +0800

    Updated sendNotif() function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 function sendNotif(notif_title, notif_body, notif_icon, notif_clicked_function) {
-  new Notification(notif_title,
-    {
-      body: notif_body,
-      icon: notif_icon
-    }
-  ).addEventListener('click', notif_clicked_function);
+  if ('Notification' in window) {
+    Notification.requestPermission().then(function(result) {
+      if (result === 'granted') {
+        new Notification(notif_title,
+          {
+            body: notif_body,
+            icon: notif_icon
+          }
+        ).addEventListener('click', notif_clicked_function);
+      }
+    });
+  }
 }
 
-if ('Notification' in window) {
-  Notification.requestPermission().then(function(result) {
-    if (result === 'granted') {
-      sendNotif("Welcome to CITreasury!", "What's up?", "COUNCIL-LOGO-removebg-preview.png", null);
-    }
-  });
-}
+sendNotif("Welcome to CITreasury!", "What's up?", "COUNCIL-LOGO-removebg-preview.png", null);
