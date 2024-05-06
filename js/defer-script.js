10b9252f306f4c581086c46f6b9361c730b1ed81
jhzrmx <dj.jasper128@gmail.com>
2024-05-06 20:30:01 +0800

    Minor changes

--- js/defer-script.js
+++ js/defer-script.js
@@ -9,3 +9,17 @@ $("#dashboard").click((e) => {
         dashboardItems.slideUp();
     }
 });
+
+$("#logout").click((e) => {
+    swal({
+        title: "Are you sure to logout?",
+        text: "You can log into your account at anytime.",
+        icon: "info",
+        buttons: true,
+        dangerMode: false,
+    }).then((willLogout) => {
+        if (willLogout) {
+            window.location.href = "../index.html";
+        }
+    });
+});
