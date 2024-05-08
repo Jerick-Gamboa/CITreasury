d95cd21dfd953e9853c6a52a37aa5413d59de8a4
jhzrmx <dj.jasper128@gmail.com>
2024-05-08 21:15:52 +0800

    Fixed logout.php errors

--- logout.php
+++ logout.php
@@ -1,6 +1,7 @@
 <?php
 session_destroy();
-setcookie('cit-emailemail', '', time() - 3600, '/');
+setcookie('cit-email', '', time() - 3600, '/');
 setcookie('cit-password', '', time() - 3600, '/');
+setcookie('cit-type', '', time() - 3600, '/');
 header("location: index.php");
 ?>
