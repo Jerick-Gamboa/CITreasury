b01aed5f503851e8fb8add439a37d7c3f5feb5d3
jhzrmx <dj.jasper128@gmail.com>
2024-05-10 21:10:11 +0800

    Added user directory

--- js/defer-script.js
+++ js/defer-script.js
@@ -61,8 +61,50 @@ menuContent = `
 </ul>
 `;
 
+menuUserContent = `
+<ul>
+    <li class="text-white font-bold">
+        <a href="index.php" id="dashboard" class="py-2 px-2 rounded transition-all duration-300-ease-in-out hover:text-custom-purplo hover:bg-white flex items-center align-middle">
+            <svg id="mdi-view-dashboard-outline" class="w-6 h-6 mr-2 fill-current" viewBox="0 0 24 24">
+                <path d="M19,5V7H15V5H19M9,5V11H5V5H9M19,13V19H15V13H19M9,17V19H5V17H9M21,3H13V9H21V3M11,3H3V13H11V3M21,11H13V21H21V11M11,15H3V21H11V15Z" />
+            </svg>
+            Dashboard
+        </a>
+        <!--ul class="pl-6 py-2" id="dashboard-items">
+            <li>
+                <a href="#" id="modify-item" class="hover:text-purple-200 flex items-center align-middle">
+                    <svg id="mdi-pencil" class="w-6 h-6 mr-2 fill-current" viewBox="0 0 24 24">
+                        <path d="M20.71,7.04C21.1,6.65 21.1,6 20.71,5.63L18.37,3.29C18,2.9 17.35,2.9 16.96,3.29L15.12,5.12L18.87,8.87M3,17.25V21H6.75L17.81,9.93L14.06,6.18L3,17.25Z" />
+                    </svg>
+                    Modify Item
+                </a>
+            </li>
+        </ul -->
+    </li>
+    <li class="text-white font-bold">
+        <a href="#" class="py-2 px-2 rounded transition-all duration-300-ease-in-out hover:text-custom-purplo hover:bg-white flex items-center align-middle">
+            <svg id="mdi-close-circle-outline" class="w-6 h-6 mr-2 fill-current" viewBox="0 0 24 24">
+                <path d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+            </svg>
+            Sanctions
+        </a>
+    </li>
+    <li class="text-white font-bold">
+        <a href="#" id="logout" class="py-2 px-2 rounded transition-all duration-300-ease-in-out hover:text-custom-purplo hover:bg-white flex items-center align-middle">
+            <svg id="mdi-logout-variant" class="w-6 h-6 mr-2 fill-current" viewBox="0 0 24 24">
+                <path d="M14.08,15.59L16.67,13H7V11H16.67L14.08,8.41L15.5,7L20.5,12L15.5,17L14.08,15.59M19,3A2,2 0 0,1 21,5V9.67L19,7.67V5H5V19H19V16.33L21,14.33V19A2,2 0 0,1 19,21H5C3.89,21 3,20.1 3,19V5C3,3.89 3.89,3 5,3H19Z" />
+            </svg>
+            Logout
+        </a>
+    </li>
+</ul>
+`;
+
 $("#menu-items").html(menuContent);
 $("#menu-items-mobile").html(menuContent);
+
+$("#menu-user-items").html(menuUserContent);
+$("#menu-user-items-mobile").html(menuUserContent);
 
 function clickToShowHide(button_id, target_show_hide) {
     const target = $(target_show_hide);
