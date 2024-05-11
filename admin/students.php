a330b3d8f507d935a40c16ea16a11c27979a5a75
jhzrmx <dj.jasper128@gmail.com>
2024-05-11 20:16:27 +0800

    Fixed missing semicolon

--- admin/students.php
+++ admin/students.php
@@ -238,7 +238,7 @@ include '../connection.php';
     </script>
     <?php
     if (isset($_POST['add-new-student'])) {
-        $sid = str_replace(" ", "", $_POST['student-id'])
+        $sid = str_replace(" ", "", $_POST['student-id']);
         $lastname = ucwords($_POST['last-name']);
         $firstname = ucwords($_POST['first-name']);
         $mi = ucwords($_POST['middle-initial']);
