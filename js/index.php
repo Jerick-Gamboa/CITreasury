24b986e9c8330aef5a1b21b4fed7084848b39594
jhzrmx <dj.jasper128@gmail.com>
2024-05-13 20:15:04 +0800

    CHanged code of index.php in /img and /js directory as simple code

--- js/index.php
+++ js/index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-<body>
-	<script type="text/javascript">
-		window.location.href = "../index.php";
-	</script>
-</body>
-</html>
+<?php
+	header("location: ../index.php");
+?>
