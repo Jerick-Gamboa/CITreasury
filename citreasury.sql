badca6e3b27b485725d4a227b685a8a541c93eac
jhzrmx <dj.jasper128@gmail.com>
2024-05-19 09:47:55 +0800

    Updated citreasury.sql for database

--- citreasury.sql
+++ citreasury.sql
@@ -20,8 +20,11 @@ CREATE TABLE `students` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 INSERT INTO `students` (`student_id`, `last_name`, `first_name`, `middle_initial`, `year_and_section`) VALUES
+('21-6394', 'Ubnlbk', 'Ihgrsfda', 'O',  '3A'),
+('22-0880', 'Menor',  'Ana Mae',  'M',  '2C'),
 ('22-1342', 'Bobis',  'Jaspher Jed',  'A',  '2C'),
-('22-1677', 'Gamboa', 'Jerick', 'D',  '2C');
+('22-1677', 'Gamboa', 'Jerick', 'D',  '2C'),
+('23-9563', 'Htdgrsg',  'Tdrgsfd',  'G',  '1E');
 
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
@@ -37,7 +40,10 @@ CREATE TABLE `accounts` (
 
 INSERT INTO `accounts` (`account_id`, `email`, `password`, `student_id`, `type`) VALUES
 (1, 'jaspherjed.bobis@cbsua.edu.ph',  'cit-22-1342',  '22-1342',  'admin'),
-(10,  'jerick.gamboa@cbsua.edu.ph', 'cit-22-1677',  '22-1677',  'user');
+(10,  'jerick.gamboa@cbsua.edu.ph', 'cit-22-1677',  '22-1677',  'user'),
+(18,  'anamae.menor@cbsua.edu.ph',  'cit-22-0880',  '22-0880',  'user'),
+(19,  'tdrgsfd.htdgrsg@cbsua.edu.ph', 'cit-23-9563',  '23-9563',  'user'),
+(20,  'ihgrsfda.ubnlbk@cbsua.edu.ph', 'cit-21-6394',  '21-6394',  'user');
 
 DROP TABLE IF EXISTS `events`;
 CREATE TABLE `events` (
@@ -46,9 +52,13 @@ CREATE TABLE `events` (
   `event_description` varchar(255) NOT NULL,
   `event_date` date NOT NULL,
   `fee_per_event` int(6) NOT NULL,
+  `sanction_fee` int(3) NOT NULL,
   PRIMARY KEY (`event_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+INSERT INTO `events` (`event_id`, `event_name`, `event_description`, `event_date`, `fee_per_event`, `sanction_fee`) VALUES
+(101, 'IT Night', 'Baraylehan', '2024-05-17', 200,  20),
+(102, 'General Assembly', 'Saaga',  '2024-05-20', 0,  30);
 
 DROP TABLE IF EXISTS `registrations`;
 CREATE TABLE `registrations` (
@@ -64,4 +74,9 @@ CREATE TABLE `registrations` (
   CONSTRAINT `registrations_student_id_students_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`student_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- 2024-05-15 10:38:53
+INSERT INTO `registrations` (`registration_id`, `event_id`, `student_id`, `registration_date`, `paid_fees`) VALUES
+(4, 101,  '22-0880',  '2024-05-15', 200),
+(10,  101,  '22-1677',  '2024-05-16', 30),
+(11,  102,  '22-1677',  '2024-05-19', 0);
+
+-- 2024-05-19 01:46:25
