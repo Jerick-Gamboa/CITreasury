0758e75de2dedc877170ed9020e4f63bc34b9021
jhzrmx <dj.jasper128@gmail.com>
2024-05-20 18:37:16 +0800

    Template implementation of sanctions.php in admin

---
+++ admin/sanctions.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<?php
+include '../connection.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="../img/nobgcitsclogo.png">
+    <!-- Import JavaScript files -->
+    <script src="../js/tailwind3.4.1.js"></script>
+    <script src="../js/tailwind.config.js"></script>
+    <script src="../js/sweetalert.min.js"></script>
+    <script src="../js/jquery-3.7.1.min.js"></script>
+    <script src="../js/apexcharts.js"></script>
+    <script src="../js/predefined-script.js"></script>
+    <script src="../js/defer-script.js" defer></script> <!-- Defer attribute means this javascript file will be executed once the HTML file is fully loaded -->
+    <title>CITreasury - Sanctions</title>
+</head>
+<body>
+    <?php
+    # Verify if login exists such that the cookie "cit-student-id" is found on browser
+    if (isset($_COOKIE['cit-student-id'])) {
+        $sql = "SELECT `type` FROM `accounts` WHERE `student_id` = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $_COOKIE['cit-student-id']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $type = $row['type'];
+            if ($type === 'user') { # If account type is user, redirect to user page
+                header("location: ../user/");
+            }
+        } else { # If account is not found, return to login page
+            header("location: ../");
+        }
+    } else { # If cookie is not found, return to login page
+        header("location: ../");
+    }
+    ?>
+    <!-- Top Navigation Bar -->
+    <nav class="fixed w-full bg-custom-purple flex flex-row shadow shadow-gray-800">
+        <img src="../img/nobgcitsclogo.png" class="w-12 h-12 my-2 ml-8">
+        <h1 class="text-3xl p-3 font-bold text-white">CITreasury</h1>
+        <div class="w-full text-white">
+            <svg id="mdi-menu" class="w-8 h-8 mr-3 my-4 p-1 float-right fill-current rounded transition-all duration-300-ease-in-out md:hidden hover:bg-white hover:text-custom-purple hover:cursor-pointer" viewBox="0 0 24 24"><path d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z" /></svg>
+        </div>
+    </nav>
+    <!-- Body -->
+    <div class="flex flex-col md:flex-row bg-custom-purplo min-h-screen">
+        <div class="mt-18 md:mt-20 mx-2">
+            <div id="menu-items" class="hidden md:inline-block w-64 h-full">
+            </div>
+        </div>
+        <div id="menu-items-mobile" class="fixed block md:hidden h-fit top-16 w-full p-4 bg-custom-purplo opacity-95">
+        </div>
+        <div class="w-full bg-red-50 px-6 min-h-screen">
+            <div class="mt-24">
+                <h1 class="text-3xl text-custom-purplo font-bold mb-5">Manage Sanctions</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
