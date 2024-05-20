2d49e182cc5b4c92c5f68c483d9995d56a0084d2
jhzrmx <dj.jasper128@gmail.com>
2024-05-21 00:58:47 +0800

    Fixed a minor query and add collect button

--- admin/eventsregistration.php
+++ admin/eventsregistration.php
@@ -122,14 +122,14 @@ include '../connection.php';
                                     CASE 
                                         WHEN `events`.`event_date` < CURDATE() AND `events`.`fee_per_event` > `registrations`.`paid_fees` 
                                         THEN `events`.`fee_per_event` + `events`.`sanction_fee` 
-                                        WHEN (`events`.`event_date` >= CURDATE() OR `events`.`fee_per_event` = `registrations`.`paid_fees`) AND `registrations`.`status` = 'FULLY_PAID_BEFORE_EVENT' 
+                                        WHEN (`events`.`event_date` >= CURDATE() OR `events`.`fee_per_event` = `registrations`.`paid_fees`) AND `registrations`.`status` = 'FULLY_PAID_BEFORE_EVENT' OR (`events`.`event_date` >= CURDATE() AND `events`.`fee_per_event` > `registrations`.`paid_fees`)
                                         THEN `events`.`fee_per_event` 
                                         ELSE `events`.`fee_per_event` + `events`.`sanction_fee` 
                                     END AS `total_fee`, 
                                     CASE 
                                         WHEN `events`.`event_date` < CURDATE() AND `events`.`fee_per_event` > `registrations`.`paid_fees` 
                                         THEN (`events`.`fee_per_event` + `events`.`sanction_fee`) - `registrations`.`paid_fees` 
-                                        WHEN (`events`.`event_date` >= CURDATE() OR `events`.`fee_per_event` = `registrations`.`paid_fees`) AND `registrations`.`status` = 'FULLY_PAID_BEFORE_EVENT' 
+                                        WHEN (`events`.`event_date` >= CURDATE() OR `events`.`fee_per_event` = `registrations`.`paid_fees`) AND `registrations`.`status` = 'FULLY_PAID_BEFORE_EVENT' OR (`events`.`event_date` >= CURDATE() AND `events`.`fee_per_event` > `registrations`.`paid_fees`)
                                         THEN `events`.`fee_per_event` - `registrations`.`paid_fees`
                                         ELSE (`events`.`fee_per_event` + `events`.`sanction_fee`) - `registrations`.`paid_fees` 
                                     END AS `balance`
@@ -187,7 +187,9 @@ include '../connection.php';
                                     <td class="px-2 border-r border-black bg-purple-100"><?php echo $totalfee; ?></td>
                                     <td class="px-2 border-r border-black bg-purple-100"><?php echo $balance; ?></td>
                                     <td class="max-w-56 bg-purple-100">
-                                        <button class="px-3 py-2 my-1 mx-1 bg-green-500 text-white text-sm font-semibold rounded-lg focus:outline-none disabled:bg-gray-400 shadow hover:bg-green-400" onclick='collect(this)' <?php if ($balance <= 0) echo 'disabled'; ?>>Collect Fee</button> <!-- Disable button if balance is zero -->
+                                        <button class="px-3 py-2 my-1 mx-1 bg-green-500 text-white text-sm font-semibold rounded-lg focus:outline-none disabled:bg-gray-400 shadow hover:bg-green-400" onclick='collect(this)' <?php if ($balance <= 0) echo 'disabled'; ?>>
+                                            <svg id="mdi-wallet-plus" class="w-4 h-4 fill-current" viewBox="0 0 24 24"><path d="M3 0V3H0V5H3V8H5V5H8V3H5V0H3M9 3V6H6V9H3V19C3 20.1 3.89 21 5 21H19C20.11 21 21 20.11 21 19V18H12C10.9 18 10 17.11 10 16V8C10 6.9 10.89 6 12 6H21V5C21 3.9 20.11 3 19 3H9M12 8V16H22V8H12M16 10.5C16.83 10.5 17.5 11.17 17.5 12C17.5 12.83 16.83 13.5 16 13.5C15.17 13.5 14.5 12.83 14.5 12C14.5 11.17 15.17 10.5 16 10.5Z" /></svg>
+                                        </button> <!-- Disable button if balance is zero -->
                                     </td>
                                 </tr>
                                 <?php
